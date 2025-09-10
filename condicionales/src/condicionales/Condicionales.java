97160d9bcf9416c5e59f89da53ec78176a9929f3
kuskus777 <u20251230601@usco.edu.co>
2025-09-10 15:00:10 -0500

    nueva modificacion

---
+++ condicionales/src/condicionales/Condicionales.java
@@ -0,0 +1,13 @@
+package condicionales;
+
+/**
+ *
+ * @author huetya
+ */
+public class Condicionales {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
